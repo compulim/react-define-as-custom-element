bbf96096abe813ffcf0230bec44a12cc7609cab4
William Wong <compulim@hotmail.com>
2024-11-18 00:27:07 -0800

    Rename

--- packages/integration-test/test/webDriver/simple/index.tsx
+++ packages/integration-test/test/webDriver/simple/index.tsx
@@ -3,4 +3,4 @@ import { defineAsCustomElement } from 'react-define-as-custom-element';
 
 const MyInput = ({ value }: { value?: string | undefined }) => <input type="text" value={value} />;
 
-defineAsCustomElement(MyInput, 'bundle--my-input', { value: 'value' });
+defineAsCustomElement(MyInput, 'simple--my-input', { value: 'value' });
