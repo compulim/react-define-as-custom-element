c272c52d906dadf0577259f523cfc216fc70daea
William Wong <compulim@users.noreply.github.com>
2024-12-06 11:40:36 +0000

    Backcompat error message

--- packages/react-define-as-custom-element/__tests__/types/noChildrenForPortal.tsx
+++ packages/react-define-as-custom-element/__tests__/types/noChildrenForPortal.tsx
@@ -5,5 +5,5 @@ const MyComponent = () => null;
 
 const { Portal } = defineAsCustomElementWithPortal(MyComponent, 'my-component', { value: 'value' });
 
-// @ts-expect-error Type '{ children: string; }' has no properties in common with type 'IntrinsicAttributes'.
+// @ts-expect-error This JSX tag's 'children' prop expects type 'never' which requires multiple children, but only a single child was provided.
 <Portal>Nothing could put here</Portal>;
